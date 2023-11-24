0c4e9e71185bd272c83a714b5a34e5a0f42837f6
J-Pinos3 <josloco665@gmail.com>
2023-11-24 11:54:31 -0500

    Handle flask messages in base.html, login and sign forms in their html files, sign up form works (still need to send data to db)

--- loginNotes/website/auth.py
+++ loginNotes/website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html', text="Testing")
+    return render_template('login.html', boolean=True)
 
 
 @auth.route('/logout')
@@ -12,6 +12,29 @@ def logout():
     return "<p> Logout </p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        data = request.form
+        email = data['email']
+        password = data['password1']
+        password2 = request.form.get('password2')
+        firstName = request.form.get('firstName')
+
+        if len(email) < 4:
+            flash('Email must be grater than 3 characters.', category="error")
+
+        elif len(firstName) < 2:
+            flash('First Name must be grater than 1 characters.', category="error")
+
+        elif password != password2:
+            flash('Passwords must be equals.', category="error")
+
+        elif len(password)< 6:
+            flash('Password must be greater than 5 characters.', category="error")
+
+        else:
+            flash('Account created!.', category="succes")
+
+        print( f"{firstName}, {email} - {password} wants to sign up." )
     return render_template('sign_up.html')
