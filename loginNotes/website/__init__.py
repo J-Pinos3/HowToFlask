cba5e88dd5dce4f1d6a6fe56dc68b4015668408e
J-Pinos3 <josloco665@gmail.com>
2023-11-26 11:02:24 -0500

    Check if user is logged in, logout function

--- loginNotes/website/__init__.py
+++ loginNotes/website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,6 +12,8 @@ def create_app():
     app.config['SECRET_KEY'] = 'fsdf56sdf 564'#SECURE COOKIES AND SESSION DATA
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+
 
     from .views import views
     from .auth import auth
@@ -23,6 +27,15 @@ def create_app():
     with app.app_context():
         db.create_all()
     #create_database(app)
+
+    login_manager = LoginManager()
+    #where we need to go if we're no logged in
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))#query instead of filter, always looks for de PK
 
     return app
 
